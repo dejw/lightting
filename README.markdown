12fc1b5d327d1606dbca15861c143c53c94a22a4
Dawid Fatyga <dawid.fatyga@gmail.com>
2010-10-02 13:24:46 +0200

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# lightting
+
+`lightting` is a simple shell script that allows you to run `lighttpd` from any
+directory and serve PHP scripts (and browsing indices).
+
+It was written for local PHP development, and it is not intended to use on a
+production.
+
+## requirements
+
+`sed` and obiously `lighttpd`
+
+## installation
+
+    wget http://github.com/dejw/lightting/raw/master/lightting.sh
+
+See `lightting.sh --help` for available options.
---
+++ lightning.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+#
+# Runs lighttpd in current directory, which allows to serve
+# PHP scripts, and browsing indices.
+#
+
+__VERSION="0.0.1"
+__AUTHOR="Dawid Fatyga"
+
+# This section defines default configuration
+: <<DEFAULT_CONFIG
+server.modules = (
+	"mod_access",
+	"mod_alias",
+	"mod_accesslog",
+)
+index-file.names           = ( "index.php", "index.html",
+                               "index.htm", "default.htm",
+                               "index.lighttpd.html" )
+url.access-deny            = ( "~", ".inc" )
+static-file.exclude-extensions = ( ".php", ".pl", ".fcgi" )
+dir-listing.encoding        = "utf-8"
+server.dir-listing          = "enable"
+
+server.username            = "www-data"
+server.groupname           = "www-data"
+
+include_shell "/usr/share/lighttpd/create-mime.assign.pl"
+DEFAULT_CONFIG
+
+function log(){
+	echo $1
+}
+
+function fatal(){
+	log "! $1"
+	exit
+}
+
+# Defines boolean command line option
+# name variable [true|false]
+function boolean(){
+	if [ -z "$3" ]; then
+		value=true
+	else
+		value=$3
+	fi
+
+	if [ "$1" = "$current" ]; then
+		export $2=$value
+	fi
+}
+
+# Defines command line option that takes 1 parameter
+# name variable
+function parameter(){
+	if [ "$1" = "$current" ]; then
+		(( i= $i + 1 ))
+		if [ $i = $arguments_length ]; then
+			fatal "$1 przyjmuje 1 parametr"
+		else
+			export $2=${arguments[$i]}
+		fi
+	fi
+}
+
+# defines command line options that calls function
+# name callback
+function callback(){
+	if [ "$1" = "$current" ]; then
+		$2
+	fi
+}
+
+function checkFor(){
+	`$1 --version &> /dev/null`
+	if [ "$?" != 0 -a "$?" != 255 ]; then
+		fatal "Cannot find $1 in your PATH"
+	fi
+	v=`$1 --version 2> /dev/null | head -n 1`
+	log "Using $v"
+}
+
+function version(){
+	echo "$__VERSION"
+	echo "Copyright (c) `date +%Y`, $__AUTHOR"
+	exit
+}
+
+function usage(){
+	echo " usage: $program [options]"
+	echo ""
+	echo " Runs lighttpd in current directory, which allows to serve PHP scripts,"
+	echo " and browsing indices."
+	echo ""
+	echo " Available options:"
+	echo "   -h --help              Shows this help"
+	echo "   -v --version           Shows version number"
+	echo ""
+	exit
+}
+
+function showDefaultConfig(){
+	checkFor sed
+	sed --silent -e '/DEFAULT_CONFIG$/,/^DEFAULT_CONFIG$/p' "$0" | sed -e '/DEFAULT_CONFIG$/d'
+	exit
+}
+
+
+OLD_IFS="$IFS";
+IFS=" "
+
+arguments_length=$#
+arguments=( $@ )
+program=$0
+
+IFS=$OLD_IFS
+
+# Process the command line arguments
+for (( i=0; i <= $#; i++ )); do
+	current=${arguments[$i]}
+
+	boolean "--force-config" force_config
+	boolean "-c" force_config
+
+	parameter "-f" config_file
+	parameter "--config-file" config_file
+
+	parameter "-p" port
+	parameter "--port" port
+
+	parameter "-b" bind
+	parameter "--bind" bind
+
+	parameter "-r" document-root
+	parameter "--document-root" document-root
+
+	callback "-v", version
+	callback "--version" version
+
+	callback "--show-default" showDefaultConfig
+	callback "-h" usage
+	callback "--help" usage
+done
+
+checkFor lighttpd
+checkFor sed
+
+__BASE__=`pwd`
+
+port=8080
+bind=localhost
+
+config_file=.lightting.conf
+force_config=true
+
+document_root=$__BASE__
+not_daemon=true
+
+chroot=/
+access="$__BASE__/access.log"
+error="$__BASE__/error.log"
+upload="$__BASE__/uploads"
+pid_file="$__BASE__/lighttpd.pid"
+
+if [ ! -e $config_file -o $force_config ]; then
+	sed --silent -e '/DEFAULT_CONFIG$/,/^DEFAULT_CONFIG$/p' "$0" | sed -e '/DEFAULT_CONFIG$/d' > $config_file
+	echo "# Config generated on `date`" >> $config_file
+	echo "server.document-root = \"$document_root\"" >> $config_file
+	echo "server.port = $port" >> $config_file
+	echo "server.bind = \"$bind\"" >> $config_file
+	echo "accesslog.filename = \"$access\"" >> $config_file
+	echo "server.errorlog = \"$error\"" >> $config_file
+	echo "server.upload-dirs = ( \"$upload\" )" >> $config_file
+	echo "server.pid-file = \"$pid_file\"" >> $config_file
+fi
+
+log "Listening on $bind:$port..."
+lighttpd -D -f $config_file
