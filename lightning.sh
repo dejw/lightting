f64e1726baa716c581f7660994f934ee8583d4b9
Dawid Fatyga <dawid.fatyga@gmail.com>
2010-10-02 13:50:28 +0200

    Converted tabs to spaces.

--- lightning.sh
+++ lightning.sh
@@ -10,10 +10,10 @@ __AUTHOR="Dawid Fatyga"
 # This section defines default configuration
 : <<DEFAULT_CONFIG
 server.modules = (
-	"mod_access",
-	"mod_alias",
-	"mod_cgi",
-	"mod_accesslog",
+  "mod_access",
+  "mod_alias",
+  "mod_cgi",
+  "mod_accesslog",
 )
 index-file.names           = ( "index.php", "index.html",
                                "index.htm", "default.htm",
@@ -30,81 +30,81 @@ include_shell "/usr/share/lighttpd/create-mime.assign.pl"
 DEFAULT_CONFIG
 
 function log(){
-	echo $1
+  echo $1
 }
 
 function fatal(){
-	log "! $1"
-	exit
+  log "! $1"
+  exit
 }
 
 # Defines boolean command line option
 # name variable [true|false]
 function boolean(){
-	if [ -z "$3" ]; then
-		value=true
-	else
-		value=$3
-	fi
+  if [ -z "$3" ]; then
+    value=true
+  else
+    value=$3
+  fi
 
-	if [ "$1" = "$current" ]; then
-		export $2=$value
-	fi
+  if [ "$1" = "$current" ]; then
+    export $2=$value
+  fi
 }
 
 # Defines command line option that takes 1 parameter
 # name variable
 function parameter(){
-	if [ "$1" = "$current" ]; then
-		(( i= $i + 1 ))
-		if [ $i = $arguments_length ]; then
-			fatal "$1 przyjmuje 1 parametr"
-		else
-			export $2=${arguments[$i]}
-		fi
-	fi
+  if [ "$1" = "$current" ]; then
+    (( i= $i + 1 ))
+    if [ $i = $arguments_length ]; then
+      fatal "$1 przyjmuje 1 parametr"
+    else
+      export $2=${arguments[$i]}
+    fi
+  fi
 }
 
 # defines command line options that calls function
 # name callback
 function callback(){
-	if [ "$1" = "$current" ]; then
-		$2
-	fi
+  if [ "$1" = "$current" ]; then
+    $2
+  fi
 }
 
 function checkFor(){
-	`$1 --version &> /dev/null`
-	if [ "$?" != 0 -a "$?" != 255 ]; then
-		fatal "Cannot find $1 in your PATH"
-	fi
-	v=`$1 --version 2> /dev/null | head -n 1`
-	log "Using $v"
+  `$1 --version &> /dev/null`
+  if [ "$?" != 0 -a "$?" != 255 ]; then
+    fatal "Cannot find $1 in your PATH"
+  fi
+  v=`$1 --version 2> /dev/null | head -n 1`
+  log "Using $v"
 }
 
 function version(){
-	echo "$__VERSION"
-	echo "Copyright (c) `date +%Y`, $__AUTHOR"
-	exit
+  echo "$__VERSION"
+  echo "Copyright (c) `date +%Y`, $__AUTHOR"
+  exit
 }
 
 function usage(){
-	echo " usage: $program [options]"
-	echo ""
-	echo " Runs lighttpd in current directory, which allows to serve PHP scripts,"
-	echo " and browsing indices."
-	echo ""
-	echo " Available options:"
-	echo "   -h --help              Shows this help"
-	echo "   -v --version           Shows version number"
-	echo ""
-	exit
+  echo " usage: $program [options]"
+  echo ""
+  echo " Runs lighttpd in current directory, which allows to serve PHP scripts,"
+  echo " and browsing indices."
+  echo ""
+  echo " Available options:"
+  echo "   -h --help              Shows this help"
+  echo "   -v --version           Shows version number"
+  echo ""
+  exit
 }
 
 function showDefaultConfig(){
-	checkFor sed
-	sed --silent -e '/DEFAULT_CONFIG$/,/^DEFAULT_CONFIG$/p' "$0" | sed -e '/DEFAULT_CONFIG$/d'
-	exit
+  checkFor sed
+  sed --silent -e '/DEFAULT_CONFIG$/,/^DEFAULT_CONFIG$/p' "$0" | sed -e '/DEFAULT_CONFIG$/d'
+  exit
 }
 
 
@@ -119,29 +119,29 @@ IFS=$OLD_IFS
 
 # Process the command line arguments
 for (( i=0; i <= $#; i++ )); do
-	current=${arguments[$i]}
+  current=${arguments[$i]}
 
-	boolean "--force-config" force_config
-	boolean "-c" force_config
+  boolean "--force-config" force_config
+  boolean "-c" force_config
 
-	parameter "-f" config_file
-	parameter "--config-file" config_file
+  parameter "-f" config_file
+  parameter "--config-file" config_file
 
-	parameter "-p" port
-	parameter "--port" port
+  parameter "-p" port
+  parameter "--port" port
 
-	parameter "-b" bind
-	parameter "--bind" bind
+  parameter "-b" bind
+  parameter "--bind" bind
 
-	parameter "-r" document-root
-	parameter "--document-root" document-root
+  parameter "-r" document-root
+  parameter "--document-root" document-root
 
-	callback "-v", version
-	callback "--version" version
+  callback "-v", version
+  callback "--version" version
 
-	callback "--show-default" showDefaultConfig
-	callback "-h" usage
-	callback "--help" usage
+  callback "--show-default" showDefaultConfig
+  callback "-h" usage
+  callback "--help" usage
 done
 
 checkFor lighttpd
@@ -166,20 +166,20 @@ upload="$__BASE__/uploads"
 pid_file="$__BASE__/lighttpd.pid"
 
 function config(){
-	echo $1 >> $config_file
+  echo $1 >> $config_file
 }
 
 if [ ! -e $config_file -o $force_config ]; then
-	sed --silent -e '/DEFAULT_CONFIG$/,/^DEFAULT_CONFIG$/p' "$0" | sed -e '/DEFAULT_CONFIG$/d' > $config_file
-	config "# Config generated on `date`"
-	config "server.document-root = \"$document_root\""
-	config "server.port = $port"
-	config "server.bind = \"$bind\""
-	config "accesslog.filename = \"$access\""
-	config "server.errorlog = \"$error\""
-	config "server.upload-dirs = ( \"$upload\" )"
-	config "server.pid-file = \"$pid_file\""
-	config "cgi.assign = ( \".php\" => \"$php_path\" )"
+  sed --silent -e '/DEFAULT_CONFIG$/,/^DEFAULT_CONFIG$/p' "$0" | sed -e '/DEFAULT_CONFIG$/d' > $config_file
+  config "# Config generated on `date`"
+  config "server.document-root = \"$document_root\""
+  config "server.port = $port"
+  config "server.bind = \"$bind\""
+  config "accesslog.filename = \"$access\""
+  config "server.errorlog = \"$error\""
+  config "server.upload-dirs = ( \"$upload\" )"
+  config "server.pid-file = \"$pid_file\""
+  config "cgi.assign = ( \".php\" => \"$php_path\" )"
 fi
 
 log "Listening on $bind:$port..."
